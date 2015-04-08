5ee1280f5db92e9c154ded615756621860a5a5bd
Ted <tedmmc@uw.edu>
2015-04-08 16:52:46 -0700

    starting work

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -1,5 +1,48 @@
 /* 
  * add your module definition for enhancedDate here
  * assign the module to a global variable
+ *
+ * excellent explaination of module method http://toddmotto.com/mastering-the-module-pattern/
+ *
+ *◦ setDate: Stores the passed-in date. Takes milliseconds after epoch or a date object. Do some 
+ *  type-checking to make sure you have a valid type. If no value is passed, default to now.
+ *◦ getDate: Returns either milliseconds after epoch by default or a date object if true is passed as an 
+ *  argument.
+ *◦ getDayName: Returns the full day name as a string, e.g. "Monday", "Tuesday", etc.
+ *◦ getMonthName: Return the full month name as a string, e.g. "January", "February", etc.
+ *◦ isFuture: Return boolean true if date is in the future (from when method is called)
+ *◦ isToday: Return boolean true if the stored date is "today".
+ *
  * /
+ 
+ 
+var Module = (function () {
+    
+    //create object
+    var myObject= {};
 
+    myObject.setDate = function(passedDate){
+        if(passedDate){
+            
+            
+        }else{
+            //nothing passed in so get current datetime
+            getDate();
+        }
+        
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+  return myObject;
+
+})();
