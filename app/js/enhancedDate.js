f4a54f624b18dead4ed09da69f6cb57ab073fc8a
Ted <tedmmc@uw.edu>
2015-04-10 15:41:28 -0700

    fixes to styling

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -1,12 +1,12 @@
-/* 
+/*
  * add your module definition for enhancedDate here
  * assign the module to a global variable
  *
  * excellent explaination of module method http://toddmotto.com/mastering-the-module-pattern/
  *
- *◦ setDate: Stores the passed-in date. Takes milliseconds after epoch or a date object. Do some 
+ *◦ setDate: Stores the passed-in date. Takes milliseconds after epoch or a date object. Do some
  *  type-checking to make sure you have a valid type. If no value is passed, default to now.
- *◦ getDate: Returns either milliseconds after epoch by default or a date object if true is passed as an 
+ *◦ getDate: Returns either milliseconds after epoch by default or a date object if true is passed as an
  *  argument.
  *◦ getDayName: Returns the full day name as a string, e.g. 'Monday', 'Tuesday', etc.
  *◦ getMonthName: Return the full month name as a string, e.g. 'January', 'February', etc.
@@ -17,78 +17,81 @@
  *  JavaScript 300 Spring 2015
  *  Ted Morrill-McClure
  */
- 
- 
-var enhancedDate = (function () {
-    
-    //create object
-    var myObject= {};
 
-    myObject.setDate = function (passedDate) {
+var enhancedDate = (function() {
+    'use strict';
+
+    //create object
+    var myObject = {};
+
+    myObject.setDate = function(passedDate) {
         var now;
-        if(passedDate){
-            if(passedDate instanceof Date){
+
+        if (passedDate) {
+            if (passedDate instanceof Date) {
                 now = passedDate;
-            }else if (typeof(passedDate) === 'number'){
+            } else if (typeof (passedDate) === 'number') {
                 now = new Date(passedDate);
-            }else {
-                now = new Date(); 
+            } else {
+                now = new Date();
             }
-        }else{
+        }else {
             //nothing passed in so get current datetime
             now = new Date();
         }
+
         myObject.now = now;
     };
 
-    myObject.getDate = function (wantObject){
-        if(wantObject==true){
+    myObject.getDate = function(wantObject) {
+        if (wantObject === true) {
             return myObject.now;
-        }else{
+        }else {
             return myObject.now.getTime();
-        }        
-    }
+        }
+    };
 
-    myObject.getDayName = function (){
-        var daysOfWeek=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-        var dayNumber= myObject.now.getDay();
+    myObject.getDayName = function() {
+        var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var dayNumber = myObject.now.getDay();
         return daysOfWeek[dayNumber];
-    }
+    };
 
-    myObject.getMonthName = function(){
+    myObject.getMonthName = function() {
         var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
   'July', 'August', 'September', 'October', 'November', 'December'];
         var monthNumber = myObject.now.getMonth();
-        return monthNames[monthNumber];
-    }
 
-    myObject.isFuture = function(){
+        return monthNames[monthNumber];
+    };
+
+    myObject.isFuture = function() {
         var currentTime = new Date();
-        if(myObject.now > currentTime){
+
+        if (myObject.now > currentTime) {
             return true;
-        }else{
+        }else {
             return false;
         }
-    }
+    };
 
-   myObject.isToday = function(){
+    myObject.isToday = function() {
         var currentTime = new Date();
         var currentYear = currentTime.getYear();
         var currentMonth = currentTime.getMonth();
         var currentDay = currentTime.getDay();
-        
+
         var objectTime = myObject.now;
         var objectYear = objectTime.getYear();
         var objectMonth = objectTime.getMonth();
         var objectDay = objectTime.getDay();
-        if(objectYear===currentYear && objectMonth===currentMonth && objectDay===currentDay){
+
+        if (objectYear === currentYear && objectMonth === currentMonth && objectDay === currentDay) {
             return true;
-        }else{
+        }else {
             return false;
         }
-    }
+    };
 
-
-  return myObject;
-
+    return myObject;
 })();
