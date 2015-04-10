b27def7f72d97cf3317ee9d40d3efc64c9bbb408
Bernie Macias <bmacias@httpstergeek.com>
2015-04-09 23:47:40 -0700

    style fixes

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -4,66 +4,84 @@
  */
 
 var enhancedDate = (function iffe() {
-  'use strict'
-    function now() {
-        var now = new Date();
-        return now;
-    }
-    var dateObjects = {
-      dateTime: now(),
-      monthNames: ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  'use strict';
+  function now() {
+    return new Date();
   }
+
+  var dateObjects = {
+    dateTime: now(),
+    monthNames: ['January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'],
+    dayNames: ['Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday']
+  };
+
   return {
-      setDate: function setDate(e) {
-          if (e instanceof Date) {
-              dateObjects.dateTime = e;
-          }
-          else if (!isNaN(e) && e > 0) {
-              dateObjects.dateTime = new Date(e);
-          }
-          else if (e === undefined){
-              dateObjects.dateTime = new Date();
-          }
-          else {
-              dateObjects.dateTime = 'argument is not istanceof Date or number';
-          }
-          return dateObjects.dateTime;
-      },
-      getDate: function getDate(e) {
-          if (e === true){
-              return dateObjects.dateTime;
-          }
-      },
-      getDayName: function getDayName() {
-          var day = dateObjects.dateTime.getDay()
-          return dateObjects.dayNames[day];
-      },
-      getMonthName: function getMonthName () {
-          var month = dateObjects.dateTime.getMonth();
-          return dateObjects.monthNames[month];
-      },
-      isFuture: function isFuture() {
-          if (now().getTime() < dateObjects.dateTime.getTime()) {
-              true;
-          }
-          else {
-              return false;
-          }
-      },
-      isToday: function isToday() {
-          var year = now().getFullYear();
-          var month = now().getMonth();
-          var day = now().getDate();
-          var nextDay = new Date(year, month, day).getTime() + 86400000;
-          var currentDay = new Date(year,month,day).getTime();
-          var time = dateObjects.dateTime.getTime();
-          if (time < nextDay && time >= currentDay) {
-              return true;
-          } else {
-              return false;
-          }
+    setDate: function setDate(e) {
+      if (e instanceof Date) {
+        dateObjects.dateTime = e;
       }
+      else if (!isNaN(e)) {
+        dateObjects.dateTime = new Date(e);
+      }
+      else if (e === undefined) {
+        dateObjects.dateTime = new Date();
+      }
+      else {
+        console.warn('argument is not instanceof Date');
+      }
+      return dateObjects.dateTime;
+    },
+    getDate: function getDate(e) {
+      if (e === true) {
+        return dateObjects.dateTime;
+      }
+    },
+    getDayName: function getDayName() {
+      var day = dateObjects.dateTime.getDay();
+      return dateObjects.dayNames[day];
+    },
+    getMonthName: function getMonthName() {
+      var month = dateObjects.dateTime.getMonth();
+      return dateObjects.monthNames[month];
+    },
+    isFuture: function isFuture() {
+      if (now().getTime() < dateObjects.dateTime.getTime()) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    },
+    isToday: function isToday() {
+      var year = now().getFullYear();
+      var month = now().getMonth();
+      var day = now().getDate();
+      var nextDay = new Date(year, month, day).getTime() + 86400000;
+      var currentDay = new Date(year, month, day).getTime();
+      var time = dateObjects.dateTime.getTime();
+      if (time < nextDay && time >= currentDay) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   };
 
 })();
