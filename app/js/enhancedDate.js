e0ee4b603bc7dfca3009d811a5a436968d2fa4d1
Jordan Bundy <jordanbundy@MacBook-Air-4.local>
2015-04-14 10:18:35 -0700

    enhancedDate module for Module_2 homework

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -1,5 +1,68 @@
 /* 
  * add your module definition for enhancedDate here
  * assign the module to a global variable
- * /
+ */
+var enhancedDate = (function enhancedDate() {
 
+    'use strict';
+
+    // Private data, not accessible outside the enhancedDate module
+    var data = {
+        date: new Date(),
+        days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
+                'Saturday'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+                'August', 'September', 'October', 'November', 'December']
+    };
+
+    // Methods availabls as a public API
+    return {
+        // setDate allows users to change the date without touching our hidden date variable
+        setDate: function setDate(dateValue) {
+
+            if (typeof dateValue === 'number' || dateValue instanceof Date) {
+                data.date = new Date(dateValue);
+            }
+        },
+        
+        // getDate returns default date if setDate has not been used
+        // if true is passed, getDate returns object instead of epoch
+        getDate: function getDate(returnObject) {
+
+            return returnObject === true ? data.date : data.date.getTime();
+        },
+
+        // getDayName returns the name of the date's day, based on the days array
+        getDayName: function getDayName() {
+
+            return data.days[data.date.getDay()];
+        },
+
+        // getMonthName returns date's month's name, based on the months array
+        getMonthName: function getMonthName() {
+
+            return data.months[data.date.getMonth()];
+        },
+
+        // isFuture compares the set date to the actual time at the moment,
+        // returns true if date is in the future
+        isFuture: function isFuture() {
+
+            return (data.date > Date.now());
+        },
+
+        // isToday sets up a function that pulls the numerical year, month and day
+        // from a Date object, then compares the set date to today's date,
+        // returns true if set date has the same year, month and day
+        isToday: function isToday() {
+
+            var today = new Date();
+
+            function makeString(startValue) {
+
+            	return '' + startValue.getFullYear() + startValue.getMonth() + startValue.getDate();
+            }
+            return (makeString(data.date) === makeString(today));
+        }
+    };
+})();
