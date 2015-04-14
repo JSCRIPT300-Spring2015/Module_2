eb82d612637978a34bad1dfbdbc3971fc6e81a30
Jordan Bundy <jordanbundy@MacBook-Air-4.local>
2015-04-14 14:32:12 -0700

    added private function to set date if it doesnt exist

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -8,39 +8,49 @@ var enhancedDate = (function enhancedDate() {
 
     // Private data, not accessible outside the enhancedDate module
     var data = {
-        date: new Date(),
         days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
                 'Saturday'],
         months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
                 'August', 'September', 'October', 'November', 'December']
     };
 
+    // check if date has been set, otherwise create a new date object
+    function hasDate() {
+        
+        if (!data.date) {
+            data.date = new Date();
+        }
+    }
+
     // Methods availabls as a public API
     return {
         // setDate allows users to change the date without touching our hidden date variable
         setDate: function setDate(dateValue) {
 
+            hasDate();
             if (typeof dateValue === 'number' || dateValue instanceof Date) {
                 data.date = new Date(dateValue);
             }
         },
         
-        // getDate returns default date if setDate has not been used
-        // if true is passed, getDate returns object instead of epoch
+        // getDate returns the value of date: epoch by default, object if true is passed
         getDate: function getDate(returnObject) {
 
+            hasDate();
             return returnObject === true ? data.date : data.date.getTime();
         },
 
         // getDayName returns the name of the date's day, based on the days array
         getDayName: function getDayName() {
 
+            hasDate();
             return data.days[data.date.getDay()];
         },
 
         // getMonthName returns date's month's name, based on the months array
         getMonthName: function getMonthName() {
 
+            hasDate();
             return data.months[data.date.getMonth()];
         },
 
@@ -48,6 +58,7 @@ var enhancedDate = (function enhancedDate() {
         // returns true if date is in the future
         isFuture: function isFuture() {
 
+            hasDate();
             return (data.date > Date.now());
         },
 
@@ -56,11 +67,12 @@ var enhancedDate = (function enhancedDate() {
         // returns true if set date has the same year, month and day
         isToday: function isToday() {
 
+            hasDate();
             var today = new Date();
 
             function makeString(startValue) {
 
-            	return '' + startValue.getFullYear() + startValue.getMonth() + startValue.getDate();
+                return '' + startValue.getFullYear() + startValue.getMonth() + startValue.getDate();
             }
             return (makeString(data.date) === makeString(today));
         }
