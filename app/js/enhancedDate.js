e67d5f073d2bd7ffd42e3e1633045c582d4c4dbe
Brice Nichols <csattler2@Christinas-MacBook-Pro.local>
2015-04-15 22:02:01 -0700

    revealing module pattern

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -1,33 +1,23 @@
-var foo = (function iife(dateInput) {
-
-    var date = new Date();
-
-    // explicit object to easily identify the API
-    return {
-            setDate: function setDate() {
-                //stores passed-in date
-                console.log(date);
-            },
-            // returns true if passed as argument
-            getDate: function getDate() {
-                console.log('getDate');
-            },
-            
-            getDayName: function getDayName() {
-            // returns full day name as a string
-                console.log('getDayName');
-            },
-
-            getMonthName: function getMonthName() {
-            // returns full month name as a string
-                console.log('getMonthName');
-            },
-            isFuture: function isFuture() {
-            // return boolean true if date is in the future
-                console.log('isFuture');
-            },
-            isToday: function isToday() {
-                console.log('isToday');
-            }
+var enhancedDate = (function (dateOne, dateTwo) {
+    var publicAPI = {};
+    
+    function getDate(test) {
+        console.log(test);
+    }
+    
+    function setDate(test2) {
+        console.log(test2);
+    }
+    function isToday(test4) {
+        console.log(test4);
+    }
+    
+    publicAPI = {
+    // Access the prive methods above
+        getDate: getDate,
+        setDate: setDate
     };
+    
+    
+    return publicAPI;
 })();
