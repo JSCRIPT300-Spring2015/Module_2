a1133156f3f21a3fcfe7b74cc426eb0706c9c493
Brice Nichols <csattler2@Christinas-MacBook-Pro.local>
2015-04-15 21:14:27 -0700

    initial

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -1,5 +1,33 @@
-/* 
- * add your module definition for enhancedDate here
- * assign the module to a global variable
- * /
+var foo = (function iife(dateInput) {
 
+    var date = new Date();
+
+    // explicit object to easily identify the API
+    return {
+            setDate: function setDate() {
+                //stores passed-in date
+                console.log(date);
+            },
+            // returns true if passed as argument
+            getDate: function getDate() {
+                console.log('getDate');
+            },
+            
+            getDayName: function getDayName() {
+            // returns full day name as a string
+                console.log('getDayName');
+            },
+
+            getMonthName: function getMonthName() {
+            // returns full month name as a string
+                console.log('getMonthName');
+            },
+            isFuture: function isFuture() {
+            // return boolean true if date is in the future
+                console.log('isFuture');
+            },
+            isToday: function isToday() {
+                console.log('isToday');
+            }
+    };
+})();
