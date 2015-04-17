df9f3f53a975eaaa264aabb31284377fe9edce63
Sam N <segid_s@yahoo.com>
2015-04-16 18:25:53 -0700

    fixing setDate() method for the last time

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -13,7 +13,8 @@ var enhancedDate = (function() {
         "June", "July", "August", "September", "October",
         "November", "December"];
     
-    //function to check and set a date if setDate wasn't called first
+    //checks if d is an instansof Date and if not then set a date when
+    //other methods are invoked before setDate() method sets the date.
     var checkAndSetDate = function () {
         if (!(d instanceof Date)) {
             d = new Date();
@@ -24,14 +25,17 @@ var enhancedDate = (function() {
     //public
     return {
         
-        setDate: function(inputDate) {            
-            var newDate = new Date(inputDate);   
-            if (inputDate instanceof Date) {
-                d = inputDate;
-            } else if (newDate instanceof Date){
-                d = newDate;                
-            } else if (!(d instanceof Date)) {
-                d = new Date();
+        setDate: function(arg = new Date()) {
+            var passedDateObj = new Date(arg);
+
+            //change input date into millisecond after epoch
+            passedInMillisecond = passedDateObj.getTime();
+        
+            if (isNaN(passedInMillisecond)) {        
+                //arg is invalid input type if passedInMillisecond is NaN
+                return alert("Invalid input date type");
+            } else {        
+                d = passedDateObj;               
             }
         },
         
@@ -71,3 +75,30 @@ var enhancedDate = (function() {
     };
     
 })();
+
+
+//test code: uncomment and run to see what the code does
+
+// when no date arg is passed to setDate method
+//enhancedDate.setDate()
+//enhancedDate.getDate(true)  //--> output: time the code is excuted
+
+// when a millisecond after epoch is passed to code
+//enhancedDate.setDate(3425787899498)
+//enhancedDate.getDate(true)  //--> output: Date {Sat Jul 23 2078 00:44:59 GMT-0700 (Pacific Standard Time)}
+
+// when a date object is passed as arg
+//enhancedDate.setDate(new Date())
+//enhancedDate.getDate()  //--> output: milliseconds after epoch of now
+
+//when a garbage arg is passed to setDate
+//enhancedDate.setDate("some string")
+//enhancedDate.getDate()  //--> output: alert--> "Invalid input date type
+
+//enhancedDate.isToday()   //-->output: true
+
+//enhancedDate.setDate(0)
+//enhancedDate.isToday()   //-->output: false
+
+
+
