fa0f1a850e37ee7e69ac4f1af3a383ed0d8e3d25
PJRice4 <pjrice4@gmail.com>
2015-04-27 19:52:05 -0700

    Assignment 2
    I apologize this is so late, I've been extremely busy at work/life.  To
    be honest, I finished this following along with you in class, so I won't
    be disappointed with a zero.  I'm trying to get caught up with the class
    this week.  See you tomorrow.
    
    Thanks,
    
    Patrick

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -1,5 +1,63 @@
-/* 
- * add your module definition for enhancedDate here
- * assign the module to a global variable
- * /
+var enhancedDate = (function() {
+	
+	'use strict'
 
+	var myDate = null;
+	var dayNames = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+	var monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+	function setDate(date) {
+		if (date instanceof Date || typeof date ==='number') {
+			myDate = new Date(date);
+		} else {
+			myDate = new Date();
+		}
+	}
+
+	function getDate(returnObj) {
+		if (returnObj) {
+			return myDate;
+		} else {
+			return myDate.getTime();
+		}
+	}
+
+	function getDayName() {
+		var day = myDate.getDay();
+		return dayNames[day];
+	}
+
+	function getMonthNames() {
+		var month = myDate.getMonth();
+		return monthNames[month];
+	}
+
+	function isToday() {
+		var today = new Date();
+		var month = today.getMonth();
+		var date = today.getDate();
+		var year = today.getYear();
+
+		if (myDate.getMonth() === month && myDate.getDate() === date && myDate.getYear() === year) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function isFuture() {
+		return myDate.getTime() > Date.now();
+	}
+
+	var publicAPI = {
+		setDate: setDate,
+		getDate: getDate,
+		getDayName: dayName,
+		getMonthName: getMonthName,
+		isFuture: isFuture,
+		isToday: isFuture
+	};
+
+	return publicAPI;
+
+})();
